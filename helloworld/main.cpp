9a3dcfe345c0fa0b1cfeb0ec06a5f6bf01e11f34
justi <justi@192.168.254.3>
2018-07-07 10:23:02 -0400

    cool2

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main()
 {
+
+	//System.out.print("Starting program");
+	cout << "Starting program..." << flush;
+
+	//System.out.println("Hello World");
 	 cout << "Hello World" << endl;
+
+	 // carriage return
+	 cout << "I'm taking over the world" << endl;
 	 return 0;
 }
