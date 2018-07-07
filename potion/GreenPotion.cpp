220abd0f6d2f343b4f9fdf3e3bb9f3d915ba28cf
justi <justi@192.168.254.3>
2018-07-07 10:29:07 -0400

    practice1

---
+++ potion/GreenPotion.cpp
@@ -0,0 +1,25 @@
+/*
+ * GreenPotion.cpp
+ *
+ *  Created on: Jul 7, 2018
+ *      Author: Joel
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	// this will not create a carriage line return! because there is no endl
+	// we won't go to a new line before outputting this
+	 cout << "this is a green potion" << endl;
+
+	 // carriage return
+	 cout << "took the green potion" << endl;
+	 return 0;
+}
+
+
